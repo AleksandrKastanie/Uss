f93dc85c62340ba2b84aa87297ff07eb3c15e377
Alex Kastanie <roof10023@gmail.com>
2022-03-27 20:26:25 +0300

    Добавлена рабочая версия Меню

---
+++ Uss/Menu.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+using System.Threading;
+
+
+namespace Uss
+{
+    internal class Menu
+    {
+        public void MainMenu()
+        { 
+            bool choose = false;
+            SetWindowSize(120,60) ;
+            do
+            {
+                Console.WriteLine(@"
+                                                                                                                                                                                                                                         
+ _______  __    _  _______  ___   _  _______    _______  _______  __   __  _______   
+|       ||  |  | ||   _   ||   | | ||       |  |       ||   _   ||  |_|  ||       |  
+|  _____||   |_| ||  |_|  ||   |_| ||    ___|  |    ___||  |_|  ||       ||    ___|  
+| |_____ |       ||       ||      _||   |___   |   | __ |       ||       ||   |___   
+|_____  ||  _    ||       ||     |_ |    ___|  |   ||  ||       ||       ||    ___|  
+ _____| || | |   ||   _   ||    _  ||   |___   |   |_| ||   _   || ||_|| ||   |___   
+|_______||_|  |__||__| |__||___| |_||_______|  |_______||__| |__||_|   |_||_______|  ");
+                Console.WriteLine(" ");
+                Console.WriteLine(@"
+ ____            ____  _     ____ ___  _  
+/  __\          /  __\/ \   /  _ \\  \//  
+|  \/|  _____   |  \/|| |   | / \| \  /   
+|  __/  \____\  |  __/| |_/\| |-|| / /    
+\_/             \_/   \____/\_/ \|/_/     
+                                           ");
+                Console.WriteLine(@"
+ _            _  _      _____ ____   
+/ \          / \/ \  /|/    //  _ \  
+| |  _____   | || |\ |||  __\| / \|  
+| |  \____\  | || | \||| |   | \_/|  
+\_/          \_/\_/  \|\_/   \____/  
+                                     ");
+                Console.WriteLine(@"
+ _____           ________  _ _  _____ 
+/  __/          /  __/\  \/// \/__ __\
+|  \    _____   |  \   \  / | |  / \  
+|  /_   \____\  |  /_  /  \ | |  | |  
+\____\          \____\/__/\\\_/  \_/  
+                                      
+                                      ");
+                var input = Console.ReadKey(); // для чтения клавишы 
+                switch (input.Key)
+                {
+                    case ConsoleKey.P:
+                        choose = true;
+                        Clear();
+                        break;
+                    case ConsoleKey.I:
+                        choose = true;
+                        Info();
+                        Clear();
+                        break;
+                    case ConsoleKey.E:
+                        choose = true;
+                        Clear(); 
+                        Exit();    
+                        break;
+                }
+                Clear();
+            } while (choose != true);
+            void Info()
+            {
+                Clear();
+                Console.WriteLine("Autor: Aleksandr Kastanie\n Kuidas mängida: \n vasak nool - madu pöörab vasakule \n parem nool - mad pöörab paremale \n allanool - madu keerab alla\n Nool üles – madu keerdub üles ");
+                Console.WriteLine("Kui madu puudutab seinu, on mäng läbi.");
+                Console.ReadKey();
+                Clear();
+                MainMenu();
+            }
+            void Exit()
+            {
+                bool answer = false;
+                do
+                {
+                    Console.WriteLine(@"Kas olete kindel, et soovite mängust väljuda?");
+                    Console.WriteLine(@"
+__   __             ___       _     
+\ \ / /            |_  |     | |    
+ \ V /   ______      | | __ _| |__  
+  \ /   |______|     | |/ _` | '_ \ 
+  | |            /\__/ / (_| | | | |
+ _\_/_           \_____ ___,_|_| |_|
+ _   _            ____  __
+| \ | |          |  ___(+)          
+|  \| |  ______  | |__  _           
+| . ` | |______| |  __|| |          
+| |\  |          | |___| |          
+\_| \_/          \____/|_|          
+                                    
+                                    ");
+
+                    var input = Console.ReadKey();
+                    switch (input.Key)
+                    {
+                        case ConsoleKey.Y:
+                            answer = true;
+                            Environment.Exit(0);
+                            break;
+                        case ConsoleKey.N:
+                            answer = true;                         
+                            break;
+                    }
+
+                } while (answer != true);
+                Clear();
+                MainMenu();
+            }
+        }
+    }
+}
--- Uss/Program.cs
+++ Uss/Program.cs
@@ -11,45 +11,50 @@ namespace Uss
 	{
 		static void Main(string[] args)
 		{
-			Console.SetBufferSize(120, 30); // задает рамку для нашей игры (меньше значения не работают) 
-
-			Walls walls = new Walls(120, 30); // Создает стены для змейки по размерам рамки (меньше значения не работают) 
-			walls.Draw();
-
-						
-			Point p = new Point(4, 5, '*');
-			Snake snake = new Snake(p, 4, Direction.RIGHT);//При запуске игры змейка начинает движение вправо из указаной точки
-			snake.Draw();
-
-			FoodCreator foodCreator = new FoodCreator(120, 30, '$'); //Создание объекта еда с условиями длина,ширина карты и символ как еда будет выглядеть
-			Point food = foodCreator.CreateFood();
-			food.Draw();
-
 			while (true)
-			{
-				if (walls.IsHit(snake) || snake.IsHitTail())//если змея врезается в стенку или хвост то программа прерывается
-				{
-					break;
-				}
-				if (snake.Eat(food))
-				{
-					food = foodCreator.CreateFood();
-					food.Draw();
-				}
-				else
-				{
-					snake.Move();
-				}
 
-				Thread.Sleep(100);//Задержка 100 милисекунд, можно поставить значения и меньше 
-				if (Console.KeyAvailable)
+
+			{
+				Menu menu = new Menu();
+				menu.MainMenu();
+				Walls walls = new Walls(50, 20); // Создает стены для змейки по размерам рамки (меньше значения не работают) 
+				walls.Draw();
+
+
+				Point p = new Point(4, 5, '*');
+				Snake snake = new Snake(p, 4, Direction.RIGHT);//При запуске игры змейка начинает движение вправо из указаной точки
+				snake.Draw();
+
+				FoodCreator foodCreator = new FoodCreator(50, 20, '$'); //Создание объекта еда с условиями длина,ширина карты и символ как еда будет выглядеть
+				Point food = foodCreator.CreateFood();
+				food.Draw();
+
+				while (true)
 				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey(key.Key);
+					if (walls.IsHit(snake) || snake.IsHitTail())//если змея врезается в стенку или хвост то программа прерывается
+					{
+						break;
+					}
+					if (snake.Eat(food))
+					{
+						food = foodCreator.CreateFood();
+						food.Draw();
+					}
+					else
+					{
+						snake.Move();
+					}
+
+					Thread.Sleep(100);//Задержка 100 милисекунд, можно поставить значения и меньше 
+					if (Console.KeyAvailable)
+					{
+						ConsoleKeyInfo key = Console.ReadKey();
+						snake.HandleKey(key.Key);
+					}
 				}
+				WriteGameOver();
+				Console.ReadLine();
 			}
-			WriteGameOver();
-			Console.ReadLine();
 		}
 
 
